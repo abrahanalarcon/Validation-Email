cb583597f3b46022a33f3ee5fa909fcf826c3190
Moises Alarcon <moiseshazim@gmail.com>
2023-12-11 17:20:18 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    
+
+    // Seleccionar los elementos de la interfaz
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spinner = document.querySelector('#spinner');
+
+    // Asignar eventos
+    inputEmail.addEventListener('blur',  validar);
+    inputAsunto.addEventListener('blur',  validar);
+    inputMensaje.addEventListener('blur',  validar);
+    
+    // function validar(e){
+    //     console.log(e.target.value);
+    // }
+
+    function validar(e){
+        if(e.target.value.trim() === ''){
+            mostrarAlerta(`El Campo ${e.target.id} es obligatorio`,e.target.parentElement);
+            return;
+        }
+
+        limpiarAlerta(e.target.parentElement);
+        
+    }
+
+    function mostrarAlerta(mensaje,referencia){
+    
+        limpiarAlerta(referencia);
+        //generar alerta 
+        const error = document.createElement('P');
+        error.textContent = mensaje;
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
+
+        //Inyectar el error al formulario 
+        referencia.appendChild(error);
+
+
+    }
+    function limpiarAlerta(referencia) {
+        // Comprueba si ya existe una alerta
+        const alerta = referencia.querySelector('.bg-red-600');
+        if(alerta) {
+            alerta.remove();
+        }
+    }
+});
